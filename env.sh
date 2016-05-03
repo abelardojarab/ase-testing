ed2d699e8008f1dffa5e97f663bb55576813185e
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-05-03 14:13:58 -0700

    Generic env.sh script

---
+++ env.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export AALSDK_GIT=$PWD/../aalsdk/
+export ASEVAL_GIT=$PWD/
+export BBB_GIT=$PWD/../BuildingBlocks/
+
+export AALUSER_DIR=$AALSDK_GIT/aaluser/
+export AALKERN_DIR=$AALSDK_GIT/aalkernel/
+export MYINST_DIR=$PWD/../myinst/
+
