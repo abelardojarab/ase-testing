8a6060de227b6c1aa293ac3f6995dd419c160640
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-05-04 07:40:57 -0700

    benchmarking script ready for primetime

--- report_nlbtest_benchmark.sh
+++ report_nlbtest_benchmark.sh
@@ -18,6 +18,10 @@ fi
 source /opt/synopsys/vcs-mx/vcs.sh
 source /opt/altera/quartus_15.sh
 
+PERF_REPORT=ase_performance.rpt
+rm -rf $PERF_REPORT
+touch $PERF_REPORT
+
 #############################################
 #               Start regression            #
 #############################################
@@ -38,8 +42,8 @@ VCS_RUN_TCL=" $ASEVAL_GIT/sim_tcl/vcs_nodumpwave.tcl $ASEVAL_GIT/sim_tcl/vcs_dum
 ASE_CFG_FILE="$ASEVAL_GIT/configs/ase_sw_simkill_cl_view_0.cfg $ASEVAL_GIT/configs/ase_sw_simkill_cl_view_1.cfg"
 
 ## Test configs
-# numcl_array="64000"
-numcl_array="64"
+numcl_array="64000"
+# numcl_array="16384"
 vc_array="0 1 2 3"
 mcl_array="0 1 3"
 
@@ -47,9 +51,6 @@ mcl_array="0 1 3"
 cd $ASEVAL_GIT/apps/
 ./build_all.sh $ASE_SRCDIR/
 cd $CURRDIR
-
-#APP_CMD="ASE_WORKDIR=$ASE_WORKDIR $ASEVAL_GIT/apps/nlb_test.out "
-#SIM_CMD=
 
 ## --------------------------------------------- ##
 for asedbg in $ASE_DEBUG
@@ -72,19 +73,20 @@ do
 	for cfg in $ASE_CFG_FILE
 	do
 	    ## --------------------------------------------- ##
-	    enable_cl_view=`grep ENABLE_CL_VIEW $simtcl`
+	    enable_cl_view=`grep ENABLE_CL_VIEW $cfg`
 	    ## --------------------------------------------- ##
 	    for numcl_set in $numcl_array ; do
 		for vc_set in $vc_array ; do
 		    for mcl_set in $mcl_array ; do
-			xterm -e "cd $ASE_WORKDIR ; ./ase_simv -ucli -do $simtcl +CONFIG=$cfg +SCRIPT=$ASE_SRCDIR/ase_regress.sh" &
+			xterm -iconic -e "cd $ASE_WORKDIR ; ./ase_simv -ucli -do $simtcl +CONFIG=$cfg +SCRIPT=$ASE_SRCDIR/ase_regress.sh" &
 			while [ ! -f $ASE_WORKDIR/.ase_ready.pid ]
 			do
 			    sleep 1
 			done
 			ASE_WORKDIR=$ASE_WORKDIR $ASEVAL_GIT/apps/nlb_test.out $numcl_set $vc_set $mcl_set > output.log
-			echo -e "ASE_DEBUG=$asedbg \tWAVEDUMP=$wavedump \t$enable_cl_view \t./nlb_test $numcl_set $vc_set $mcl_set\t"
-			grep -i "elapsed" output.log 
+			simtime=`grep -i nsec output.log`
+			echo -e "ASE_DEBUG=$asedbg \tWAVEDUMP=$wavedump \t$enable_cl_view \t./nlb_test $numcl_set $vc_set $mcl_set \t $simtime" >> $PERF_REPORT
+			sleep 3
 		    done
 		done
 	    done
