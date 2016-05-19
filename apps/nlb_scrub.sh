f2833013c4ddbc0bfd5ff5a06848a31505edc337
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-05-19 08:50:48 -0700

    nlb quick scrub test script and build script changes

--- apps/nlb_scrub.sh
+++ apps/nlb_scrub.sh
@@ -1,31 +1,25 @@
 #!/bin/sh
 
-rm output.*.log
-./build_all.sh
+rm scrub.*.log
+#./build_all.sh
 
 vc_arr="0 1 2 3"
 
 mcl_arr="0 1 3"
 
-cl_arr="16 256 1024"
+cl_arr="16 64 256 1024 4096"
 
 echo "NLB Scrub test will run" $NUM_TESTS "tests"
 for vc in $vc_arr; do
     for mcl in $mcl_arr; do
 	for cl in $cl_arr; do
-	    timeout 20 ./nlb_test.out $cl $vc $cl > nlb-out.log
+	    timeout 300 ./nlb_test.out $cl $vc $cl > scrub_test_"$cl"_"$vc"_"$mcl".log
 	    echo "./nlb_test $cl $vc $mcl "
-	    if [ $? == 124 ];
+	    if [ $? != 0 ];
 	    then
-	    	echo "Timeout occured "
-	    else
-	    	if grep -q "ERROR" nlb-out.log
-	    	then
-	    	    echo " FAIL "
-	    	else
-	    	    echo " OK   "
-	    	fi
-	    fi
+	    	echo " ERROR "
+		exit 1
+	    fi	    
 	done
     done
 done
