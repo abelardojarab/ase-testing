142e54e42e428ab2c7eddd2cd6598c4b84345cbd
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-05-19 08:38:41 -0700

    Stress test and NLB modified to crash reasonably on stress tests

--- apps/stress.sh
+++ apps/stress.sh
@@ -2,7 +2,7 @@
 
 rm output.*.log
 
-./build.sh
+./build_all.sh
 
 vc_arr[0]="0"
 vc_arr[1]="1"
@@ -37,18 +37,19 @@ do
 	mcl_set=${mcl_arr[$index]}
 	mcl_cnt=$(($mcl_set + 1))
 
-	num_cl=`shuf -i 12000-16000 -n 1`
-#	num_cl=`shuf -i 256-1024 -n 1`
+#	num_cl=`shuf -i 12000-16000 -n 1`
+	num_cl=`shuf -i 256-1024 -n 1`
 #	num_cl=`shuf -i 4-16 -n 1`
 	num_cl=$(($num_cl * $mcl_cnt))
 	
 	echo ./nlb_test.out $num_cl $vc_set $mcl_set
-#	./nlb_test $num_cl $vc_set $mcl_set > output.$i.log
-	./nlb_test.out $num_cl $vc_set 0 > output.$i.log
-	if grep -q "ERROR" output.$i.log
+	./nlb_test.out $num_cl $vc_set $mcl_set > output.$i.log
+	if [ "$?" != 0 ] ; 
 	then
 	    echo "***** Test error *****"
 	    exit
+	else
+	    echo "Test PASS"
 	fi
 	sleep 1
     else
