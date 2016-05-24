0b3efbd0ff3a650f334046009915c7921c3d9d68
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-05-24 10:43:18 -0700

    Stress test run is now ./stress.sh <num_tests> <short|long>

--- apps/stress.sh
+++ apps/stress.sh
@@ -22,7 +22,21 @@ else
     NUM_TESTS=$1
 fi
 
-echo "Stress test will run" $NUM_TESTS "tests"
+if [ -z "$1" ]
+then
+    echo "Usage: ./stress.sh <num_tests> <short|long>"
+    exit
+else
+    TEST_TYPE=$2
+    if [ "$2" != "short" ]; then
+	if [ "$2" != "long" ]; then
+	    echo "TEST_TYPE must be small OR long"
+	    exit
+	fi
+    fi	
+fi
+
+echo "Stress test will run $NUM_TESTS tests"
 for i in `seq 1 $NUM_TESTS`;
 do
     if pgrep "ase_simv" -u $USER
@@ -37,9 +51,13 @@ do
 	mcl_set=${mcl_arr[$index]}
 	mcl_cnt=$(($mcl_set + 1))
 
-#	num_cl=`shuf -i 12000-16000 -n 1`
-	num_cl=`shuf -i 256-1024 -n 1`
-#	num_cl=`shuf -i 4-16 -n 1`
+	if [ $TEST_TYPE == "long" ] ; then
+	    num_cl=`shuf -i 12000-16000 -n 1`	    
+	fi
+	if [ $TEST_TYPE == "short" ] ; then
+	    num_cl=`shuf -i 256-1024 -n 1`
+	fi
+
 	num_cl=$(($num_cl * $mcl_cnt))
 	
 	echo ./nlb_test.out $num_cl $vc_set $mcl_set
@@ -47,6 +65,7 @@ do
 	if [ "$?" != 0 ] ; 
 	then
 	    echo "***** Test error *****"
+	    $ASEVAL_GIT/kill_running_ase.sh
 	    exit
 	else
 	    echo "Test PASS"
@@ -54,9 +73,11 @@ do
 	sleep 1
     else
 	echo "Simulator not running... EXIT";
+	$ASEVAL_GIT/kill_running_ase.sh
 	exit
     fi
 done
 echo "------------------------------------------------"
-# pkill ase_simv
+
+$ASEVAL_GIT/kill_running_ase.sh
 
