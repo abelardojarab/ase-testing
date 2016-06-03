f5136f7b17847d06d63deabce2f3dc6ad45524f4
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-06-03 13:43:12 -0700

    Commited more changes to brute-force VCS test - scrubs major versions from 2011 to 2016

--- brute-force-ec-vcs-test.sh
+++ brute-force-ec-vcs-test.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+## NOTES:
+# Do not list any older than 2011
+# Do not list any with "Beta" in the name
+# Do not list all the "-x" version, just list the largest one
+
 source ~/bashrc_aliases/quartus_15_pro.csh
 
 # export CURRDIR=$PWD
@@ -9,32 +14,47 @@ source ~/bashrc_aliases/quartus_15_pro.csh
 export SNPSLMD_LICENSE_FILE="26586@plxs0402.pdx.intel.com:26586@plxs0405.pdx.intel.com:26586@plxs0406.pdx.intel.com:26586@plxs0408.pdx.intel.com:26586@plxs0414.pdx.intel.com:26586@plxs0415.pdx.intel.com:26586@plxs0416.pdx.intel.com:26586@plxs0418.pdx.intel.com"
 
 export TOOL_VERSION="
+/p/eda/acd/synopsys/vcs/E-2011.03-SP1-7 \
+/p/eda/acd/synopsys/vcs/F-2011.12-SP1 \
+/p/eda/acd/synopsys/vcs/F-2011.12-SP1-8 \
+/p/eda/acd/synopsys/vcs/G-2012.09 \
+/p/eda/acd/synopsys/vcs/H-2013.06-SP1-2 \
+/p/eda/acd/synopsys/vcs/I-2014.03 \
+/nfs/site/eda/tools/synopsys/vcs/E-2011.03-SP1-16 \
+/nfs/site/eda/tools/synopsys/vcs/F-2011.12-SP1-11 \
+/nfs/site/eda/tools/synopsys/vcs/G-2012.09 \
+/nfs/site/eda/tools/synopsys/vcs/H-2013.06-4 \
+/nfs/site/eda/tools/synopsys/vcs/H-2013.06-SP1-6 \
+/nfs/site/eda/tools/synopsys/vcs/H-2013.06-SP1 \
+/nfs/site/eda/tools/synopsys/vcs/I-2014.03 \
 /p/eda/acd/synopsys/vcsmx/F-2011.12-SP1-11 \
-/p/eda/acd/synopsys/vcsmx/F-2011.12-SP1-5 \
-/p/eda/acd/synopsys/vcsmx/F-2011.12-SP1-8 \
 /p/eda/acd/synopsys/vcsmx/H-2013.06 \
 /p/eda/acd/synopsys/vcsmx/H-2013.06-3 \
 /p/eda/acd/synopsys/vcsmx/H-2013.06-SP1 \
 /p/eda/acd/synopsys/vcsmx/H-2013.06-SP1-11 \
-/p/eda/acd/synopsys/vcsmx/H-2013.06-SP1-15-B-12 \
-/p/eda/acd/synopsys/vcsmx/H-2013.06-SP1-2 \
-/p/eda/acd/synopsys/vcsmx/H-2013.06-SP1-6_Full64 \
-/p/eda/acd/synopsys/vcsmx/H-2013.06-SP1-9 \
-/p/eda/acd/synopsys/vcsmx/H-2013.06.SP1-9 \
 /p/eda/acd/synopsys/vcsmx/I-2014.03 \
 /p/eda/acd/synopsys/vcsmx/I-2014.03-1 \
-/p/eda/acd/synopsys/vcs/D-2010.06-SP1-3-B-2 \
-/p/eda/acd/synopsys/vcs/E-2011.03-SP1-4 \
-/p/eda/acd/synopsys/vcs/E-2011.03-SP1-7 \
-/p/eda/acd/synopsys/vcs/F-2011.12-SP1 \
-/p/eda/acd/synopsys/vcs/F-2011.12-SP1-3 \
-/p/eda/acd/synopsys/vcs/F-2011.12-SP1-5 \
-/p/eda/acd/synopsys/vcs/F-2011.12-SP1-8 \
-/p/eda/acd/synopsys/vcs/G-2012.09 \
-/p/eda/acd/synopsys/vcs/H-2013.06-Beta1 \
-/p/eda/acd/synopsys/vcs/H-2013.06-SP1-1 \
-/p/eda/acd/synopsys/vcs/H-2013.06-SP1-2 \
-/p/eda/acd/synopsys/vcs/I-2014.03 \
+/nfs/site/eda/tools/synopsys/vcsmx/E-2011.03-3 \
+/nfs/site/eda/tools/synopsys/vcsmx/E-2011.03-SP1-11 \
+/nfs/site/eda/tools/synopsys/vcsmx/E-2011.03 \
+/nfs/site/eda/tools/synopsys/vcsmx/F-2011.12-4 \
+/nfs/site/eda/tools/synopsys/vcsmx/F-2011.12-SP1-28 \
+/nfs/site/eda/tools/synopsys/vcsmx/F-2011.12 \
+/nfs/site/eda/tools/synopsys/vcsmx/G-2012.09-3 \
+/nfs/site/eda/tools/synopsys/vcsmx/G-2012.09-SP1-7 \
+/nfs/site/eda/tools/synopsys/vcsmx/G-2012.09-SP1 \
+/nfs/site/eda/tools/synopsys/vcsmx/G-2012.09 \
+/nfs/site/eda/tools/synopsys/vcsmx/H-2013.06-4 \
+/nfs/site/eda/tools/synopsys/vcsmx/H-2013.06-SP1-15 \
+/nfs/site/eda/tools/synopsys/vcsmx/H-2013.06 \
+/nfs/site/eda/tools/synopsys/vcsmx/I-2014.03 \
+/nfs/site/eda/tools/synopsys/vcsmx/J-2014.09-SP3-3 \
+/nfs/site/eda/tools/synopsys/vcsmx/J-2014.12-1 \
+/nfs/site/eda/tools/synopsys/vcsmx/J-2014.12-SP3 \
+/nfs/site/eda/tools/synopsys/vcsmx/J-2014.12 \
+/nfs/site/eda/tools/synopsys/vcsmx/K-2015.09-SP1 \
+/nfs/site/eda/tools/synopsys/vcsmx/K-2015.09 \
+/nfs/site/eda/tools/synopsys/vcsmx/L-2016.06-Beta \
 "
 
 cd $ASE_SRCDIR
@@ -67,15 +87,14 @@ for i in $TOOL_VERSION; do
 	if [ $? -eq 0 ]; then
     	    echo -e "$VCS_HOME" "\t\t" "[RUN PASS]" >> $ASEVAL_GIT/vcs-scrub.txt	    
 	else
-	    echo -e "$VCS_HOME" "\t\t" "[RUN FAIL]" >> $ASEVAL_GIT/vcs-scrub.txt	    
+	    echo -e "$VCS_HOME" "\t\t" "[** RUN FAIL **]" >> $ASEVAL_GIT/vcs-scrub.txt	    
 	fi
 	$ASEVAL_GIT/kill_running_ase.sh
 	sleep 1
     else
-    	echo -e "$VCS_HOME" "\t\t" "[BUILD FAIL]" >> $ASEVAL_GIT/vcs-scrub.txt
+    	echo -e "$VCS_HOME" "\t\t" "[** BUILD FAIL **]" >> $ASEVAL_GIT/vcs-scrub.txt
 	mv log.txt $logname-build-FAIL.txt
-	# make clean
-	# make | tee
     fi
 done
+
 
