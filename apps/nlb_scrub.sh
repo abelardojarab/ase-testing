3bd739656e7a2b4a97e8044134873bdb534fe12f
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-06-03 19:09:38 -0700

    nlb_scrub checks if ASE is running

--- apps/nlb_scrub.sh
+++ apps/nlb_scrub.sh
@@ -12,8 +12,10 @@ else
     vc_arr="0 1 2"
     mcl_arr="0 3"
     cl_arr="16 4096"
-    timeout=200
+    timeout=300
 fi
+
+ase_pid=`cat $ASE_WORKDIR/.ase_ready.pid | grep pid | cut -d "=" -s -f2-`
 
 for vc in $vc_arr; do
     for mcl in $mcl_arr; do
@@ -24,8 +26,16 @@ for vc in $vc_arr; do
 	    then
 	    	echo " ERROR running nlb_scrub -- EXIT"
 		exit -1
-	    fi	    
+	    fi	    	    
+	    if ps -p $ase_pid > /dev/null
+	    then
+		echo "Simulator is running"
+	    else
+		echo "ASE PID seems to have crashed -- EXIT"
+		exit -1
+	    fi
 	done
     done
 done
+
 
