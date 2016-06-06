b95792011d905e1fc4e5cf8abe6307b142132b61
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-06-06 15:40:32 -0700

    fixing paths in brute-questa

--- brute-force-ec-questa-test.sh
+++ brute-force-ec-questa-test.sh
@@ -9,49 +9,49 @@ export LM_LICENSE_FILE="1717@mentor04p.elic.intel.com":$LM_LICENSE_FILE
 export LM_PROJECT="ATP-PLAT-DEV"
 
 export TOOL_VERSION="
-/p/eda/acd/mentor/questasim/6.6a/bin \
-/nfs/site/eda/tools/mentor/questasim/10.0c/linux_2.6.16_x86-64/bin/ \
-/nfs/site/eda/tools/mentor/questasim/10.1a/common/bin \
-/nfs/site/eda/tools/mentor/questasim/10.1b/common/bin \
-/nfs/site/eda/tools/mentor/questasim/10.1c/common/bin \
-/nfs/site/eda/tools/mentor/questasim/10.2b/linux_2.6.16_x86-64/bin \
-/nfs/site/eda/tools/mentor/questasim/10.2c/common/bin \
-/nfs/site/eda/tools/mentor/questasim/10.2e/linux_3.0.51_x86-64/bin \
-/nfs/site/eda/tools/mentor/questasim/10.3c/common/bin \
-/nfs/site/eda/tools/mentor/questasim/6.2f/common/bin \
-/nfs/site/eda/tools/mentor/questasim/6.3c/common/bin \
-/nfs/site/eda/tools/mentor/questasim/6.3f/common/bin \
-/nfs/site/eda/tools/mentor/questasim/6.3g/common/bin \
-/nfs/site/eda/tools/mentor/questasim/6.3h/common/bin \
-/nfs/site/eda/tools/mentor/questasim/6.3i/common/bin \
-/nfs/site/eda/tools/mentor/questasim/6.4/common/bin \
-/nfs/site/eda/tools/mentor/questasim/6.5b/common/bin \
-/nfs/site/eda/tools/mentor/questasim/6.6b/linux_2.6.5_x86-64/questasim/bin \
+/p/eda/acd/mentor/questasim/6.6a/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/10.0c/linux_2.6.16_x86-64/linux_x86_64/ \
+/nfs/site/eda/tools/mentor/questasim/10.1a/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/10.1b/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/10.1c/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/10.2b/linux_2.6.16_x86-64/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/10.2c/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/10.2e/linux_3.0.51_x86-64/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/10.3c/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/6.2f/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/6.3c/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/6.3f/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/6.3g/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/6.3h/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/6.3i/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/6.4/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/6.5b/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/questasim/6.6b/linux_2.6.5_x86-64/questasim/linux_x86_64 \
 /nfs/site/eda/tools/mentor/questasim/6.6f/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.0b/common/modeltech/bin/ \
-/nfs/site/eda/tools/mentor/modelsim/6.0c/common/modeltech/bin/ \
-/nfs/site/eda/tools/mentor/modelsim/6.0d/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.1a/common/modeltech/bin/ \
-/nfs/site/eda/tools/mentor/modelsim/6.1b/common/modeltech/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.1d/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.1e/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.1f/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.1g/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.2/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.2b/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.2c/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.2d/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.2e/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.2g/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.2i/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.3/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.3a/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.3e/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.3g/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.3i/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.4a/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.6b1/common/bin \
-/nfs/site/eda/tools/mentor/modelsim/6.6c/common/bin \
+/nfs/site/eda/tools/mentor/modelsim/6.0b/common/modeltech/linux_x86_64/ \
+/nfs/site/eda/tools/mentor/modelsim/6.0c/common/modeltech/linux_ia64/ \
+/nfs/site/eda/tools/mentor/modelsim/6.0d/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.1a/common/modeltech/linux_x86_64/ \
+/nfs/site/eda/tools/mentor/modelsim/6.1b/common/modeltech/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.1d/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.1e/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.1f/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.1g/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.2/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.2b/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.2c/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.2d/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.2e/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.2g/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.2i/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.3/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.3a/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.3e/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.3g/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.3i/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.4a/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.6b1/common/linux_x86_64 \
+/nfs/site/eda/tools/mentor/modelsim/6.6c/common/linux_x86_64 \
 "
 
 cd $ASE_SRCDIR
@@ -67,29 +67,31 @@ for i in $TOOL_VERSION; do
     echo "------------------------------------------------------------------"
     echo "Now running : $MTI_HOME "
     echo "------------------------------------------------------------------"    
+    # ls $i
+    # which vlog
     cd $ASE_SRCDIR
     make clean
     make
     echo -e -n "$MTI_HOME" >> $ASEVAL_GIT/$SCRUB_LOG
     if [ -f $ASE_SRCDIR/work/work/dpiheader.h ]; then
     	echo -e -n "\t[BUILD PASS]" >> $ASEVAL_GIT/$SCRUB_LOG
-	echo "Running tests"
-	xterm -iconic -e "cd $ASE_SRCDIR ; make sim " &
-#	xterm -e "cd $ASE_SRCDIR ; make sim " &
-	while [ ! -f $ASE_WORKDIR/.ase_ready.pid ]
-	do
-	    sleep 1
-	done
-	cd $ASEVAL_GIT/apps/
-	./nlb_scrub.sh 
-	if [ $? -eq 0 ]; then
+    	echo "Running tests"
+#    	xterm -iconic -e "cd $ASE_SRCDIR ; make sim " &
+    	xterm -e "cd $ASE_SRCDIR ; make sim " &
+    	while [ ! -f $ASE_WORKDIR/.ase_ready.pid ]
+    	do
+    	    sleep 1
+    	done
+    	cd $ASEVAL_GIT/apps/
+    	./nlb_scrub.sh 
+    	if [ $? -eq 0 ]; then
     	    echo -e -n "\t[RUN PASS]" >> $ASEVAL_GIT/$SCRUB_LOG	    
-	    $ASEVAL_GIT/kill_running_ase.sh
-	else
-	    echo -e -n "\t[** RUN FAIL **]" >> $ASEVAL_GIT/$SCRUB_LOG
-	    $ASEVAL_GIT/kill_running_ase.sh
-	fi
-	sleep 1
+    	    $ASEVAL_GIT/kill_running_ase.sh
+    	else
+    	    echo -e -n "\t[** RUN FAIL **]" >> $ASEVAL_GIT/$SCRUB_LOG
+    	    $ASEVAL_GIT/kill_running_ase.sh
+    	fi
+    	sleep 1
     else
     	echo -n -e "\t[** BUILD FAIL **]" >> $ASEVAL_GIT/$SCRUB_LOG
     fi
