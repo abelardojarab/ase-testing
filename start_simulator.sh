af5bc9fcd6e3d210e8c85f624ac405319055fca6
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-06-09 17:48:03 -0700

    Start_simulator script added

---
+++ start_simulator.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [[ "$1" == "" ]];
+then
+    echo "** ERROR : Incorrect usage ! **"
+    echo "Usage: ./start_simulator.sh <VCS|QUESTA>"
+    exit 1
+elif [[ "$1" != "VCS" ]]; 
+then
+    if [[ "$1" != "QUESTA" ]];
+    then
+	echo "** ERROR : Incorrect option ! **"
+	echo "Usage: ./start_simulator.sh <VCS|QUESTA>"
+	exit 1
+    fi
+fi
+echo "Starting simulator ..."
+cd $ASE_SRCDIR
+make sim SIMULATOR=$1
