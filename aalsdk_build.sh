b7107c869689e1fd7cef08bcf1de439730573819
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-06-13 17:45:46 -0700

    removed generic build.sh, edited aalsdk_build.sh to build Hello_ALI_NLB.cpp

--- aalsdk_build.sh
+++ aalsdk_build.sh
@@ -37,4 +37,8 @@ cd mybuild
 ../configure --prefix=$MYINST_DIR || exit 1
 make -j 8  || exit 1
 
+cd $AALSAMP_DIR/Hello_ALI_NLB/SW/
+sed -i 's/#define  HWAFU/\/\/#define  HWAFU/g' HelloALINLB.cpp
+sed -i 's/\/\/#define  ASEAFU/#define  ASEAFU/g' HelloALINLB.cpp
+make prefix=$MYINST_DIR || exit 1
 
