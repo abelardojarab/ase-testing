e0886092db16c05d046f95918b8c891179be6394
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-06-23 16:14:44 -0700

    Rebuild AALSDK script for local rebuilds only

---
+++ rebuild_aalsdk_only.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -v
+
+if [ -z "$AALSDK_GIT" ]; then
+    echo "env(AALSDK_GIT) has not been set !"
+    exit 1
+fi
+
+if [ -z "$BBB_GIT" ]; then
+    echo "env(BBB_GIT) has not been set !"
+    exit 1
+fi
+
+if [ -z "$ASEVAL_GIT" ]; then
+    echo "env(ASEVAL_GIT) has not been set !"
+    exit 1
+fi
+
+
+cd $AALUSER_DIR/mybuild
+make -j 8 || exit 1
+make install || exit 1
+
+cd $MYINST_DIR/lib/
+objdump -S libASE.so > libASE.obj
+
