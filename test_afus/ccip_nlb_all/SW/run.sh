21c07ac3cf0197a256fbc560d362c3b771196a88
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-07-20 10:02:34 -0700

    Added run.sh tests for BDX2-trput

--- test_afus/ccip_nlb_all/SW/run.sh
+++ test_afus/ccip_nlb_all/SW/run.sh
@@ -34,9 +34,9 @@ then
     # $ASEVAL_GIT/apps/
     # ./build_all.sh
     # ./nlb_scrub.sh full
-    echo "#############################################"
-    echo "#        Testing fpgadiag on $RELCODE       #"
-    echo "#############################################"
+    echo "######################################################"
+    echo "#        Testing fpgadiag on $RELCODE with lpbk1"
+    echo "######################################################"
     ## Listing options
     fpgadiag_vc_arr="--va --vl0 --vh0 --vh1"
     fpgadiag_mcl_arr="1 2 4"
@@ -53,8 +53,7 @@ then
     			date
     			if ps -p $ase_pid > /dev/null
     			then
-    			    echo "./fpgadiag --target=ase --mode=lpbk1 --begin=$cnt_set $rd_set $wr_set --mcl=$mcl_set $vc_set"
-    			    timeout 1800 ./fpgadiag --target=ase --mode=lpbk1 --begin=$cnt_set $rd_set $wr_set --mcl=$mcl_set $vc_set
+    			    timeout 600 ./fpgadiag --target=ase --mode=lpbk1 --begin=$cnt_set $rd_set $wr_set --mcl=$mcl_set $vc_set
 			    errcode=$?
     			    if [[ $errcode != 0 ]] 
     			    then
@@ -72,6 +71,45 @@ then
     	    done
     	done
     done
+
+    echo "#######################################################"
+    echo "#        Testing fpgadiag on $RELCODE with trput"
+    echo "#######################################################"
+    ## Listing options
+    fpgadiag_vc_arr="--va --vl0 --vh0 --vh1"
+    fpgadiag_mcl_arr="1 2 4"
+    fpgadiag_cnt_arr="64 1024 8192"
+    fpgadiag_rdtype_arr="--rds --rdi"
+    fpgadiag_wrtype_arr="--wt --wb"
+    ## Run options
+    cd $MYINST_DIR/bin
+    for vc_set in $fpgadiag_vc_arr ; do
+    	for mcl_set in $fpgadiag_mcl_arr ; do
+    	    for cnt_set in $fpgadiag_cnt_arr ; do
+    		for rd_set in $fpgadiag_rdtype_arr ; do
+    		    for wr_set in $fpgadiag_wrtype_arr ; do
+    			date
+    			if ps -p $ase_pid > /dev/null
+    			then
+    			    timeout 600 ./fpgadiag --target=ase --mode=trput --begin=$cnt_set $rd_set $wr_set --mcl=$mcl_set $vc_set --timeout-sec=20 --cont
+			    errcode=$?
+    			    if [[ $errcode != 0 ]] 
+    			    then
+				echo "fpgadiag timed out -- FAILURE EXIT, Error code $errcode !!"
+				errno $errcode
+				echo "Last command:  ./fpgadiag --target=ase --mode=trput --begin=$cnt_set $rd_set $wr_set --mcl=$mcl_set $vc_set --timeout-sec=20 --cont"
+    				exit 1
+    			    fi
+    			else
+    			    echo "** Simulator not running **"
+    			    exit 1
+    			fi
+    		    done
+    		done
+    	    done
+    	done
+    done
+
 fi
 
 #######################################################################
