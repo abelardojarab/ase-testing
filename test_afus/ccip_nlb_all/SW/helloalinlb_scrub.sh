567fb61afcaa70eb5e5a9e97fb7f839aae5f793f
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-08-08 14:34:15 -0700

    Hello_ALI_NLB scrub test

---
+++ test_afus/ccip_nlb_all/SW/helloalinlb_scrub.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+LOGNAME="$PWD/test_status.log"
+
+# Delete log if exists
+rm -rf $LOGNAME
+
+# Simulator PID
+ase_pid=`cat $ASE_WORKDIR/.ase_ready.pid | grep pid | cut -d "=" -s -f2-`
+
+# Return code
+retcode=0
+
+# Change Directory to Hello_ALI_NLB
+cd $AALSAMP_DIR/Hello_ALI_NLB/SW/
+
+# Run 
+./helloALInlb
+
+# Record Retcode
+retcode=$?
+
+## Return status
+if [ $retcode == "0" ]
+then
+    echo "Hello_ALI_NLB completed -- SUCCESS"
+else
+    echo "Hello_ALI_NLB completed -- FAILED"
+    exit 1
+fi
