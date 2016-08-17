c4d2ee4a5c27c783c33b6759b96ad1d63104bb06
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-08-17 16:24:16 -0700

    apps: run.sh changed to valgrind.sh

--- apps/valgrind.sh
+++ apps/valgrind.sh
@@ -1,18 +1,29 @@
 #!/bin/sh
 
+# Wait for Simulator to be running/ready
+echo "Waiting for simulator to be ready ... "
+for sleep in `seq 0 180`;
+do
+    if [ ! -f $ASE_WORKDIR/.ase_ready.pid ]
+    then
+	sleep 1
+    fi
+done
+
+# Run valggrind app
 valgrind -v --tool=memcheck \
     --leak-check=full \
     --track-origins=yes \
     --num-callers=20 \
     --error-limit=no \
-    --log-file=run.log \
+    --log-file=valgrind.log \
     --track-fds=yes \
     --leak-check=full \
     --track-origins=yes \
     --show-reachable=yes \
     --show-leak-kinds=definite,possible \
     --undef-value-errors=yes \
-    ./mmio_test.out
+    ./nlb_test.out
 
-#    ./nlb_test.out 1024 0 3
+# 1024 0 3
 
