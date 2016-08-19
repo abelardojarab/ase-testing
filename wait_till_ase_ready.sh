f0ffd5c51d6237930d1295006369efd0fb5982f7
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-08-18 22:21:38 -0700

    wait ASE script waits infinitely

--- wait_till_ase_ready.sh
+++ wait_till_ase_ready.sh
@@ -2,17 +2,9 @@
 
 # Wait for Simulator to be running/ready
 echo "Waiting for simulator to be ready (will wait > 10 mins) ... "
-for sleep in `seq 0 600`;
+while [ ! -f $ASE_WORKDIR/.ase_ready.pid ]
 do
-    if [ ! -f $ASE_WORKDIR/.ase_ready.pid ]
-    then
-	sleep 1
-    fi
+    sleep 1
 done
-if [ ! -f $ASE_WORKDIR/.ase_ready.pid ]
-then
-    echo "Simulator might probably not come up at all -- ending regression here !"
-    exit 1
-fi
 echo "DONE"
 
