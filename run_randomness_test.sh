a50f8a239b367527e0b93299c5be386e315cc879
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-09-08 08:39:55 -0700

    Mods to randomness_test, and a change to fpgadiag_include: --rds is temporarily unavailable

--- run_randomness_test.sh
+++ run_randomness_test.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 NUM_RUNS=2
-
 ASE_CONFIG=$ASEVAL_GIT/test.cfg
+DEBUG=0
 
 ./config_generator.sh single 1234 silent 300.000 64 > $ASE_CONFIG
 
 ## Build simulator
 cd $ASE_SRCDIR
-make ASE_DEBUG=0
+make ASE_DEBUG=$DEBUG
 
 for ii in `seq 1 $NUM_RUNS`
 do
@@ -16,11 +16,11 @@ do
     echo $ii
     ## Run simulator
     cd $ASE_SRCDIR
-    make sim ASE_DEBUG=0 ASE_CONFIG=$ASE_CONFIG &
+    make sim ASE_DEBUG=$DEBUG ASE_CONFIG=$ASE_CONFIG &
     $ASEVAL_GIT/wait_till_ase_ready.sh
     ## Run application
     cd $MYINST_DIR/bin/
-    ./fpgadiag --target=ase --mode=lpbk1 --begin=32768
+    ./fpgadiag --target=ase --mode=lpbk1 --begin=64 --mcl=4
     ## Wait until Simulator shuts down
     while [ -f $ASE_WORKDIR/.ase_ready.pid ]
     do
@@ -30,3 +30,6 @@ do
     cat $ASE_WORKDIR/ccip_transactions.tsv | cut -s -f2- | grep -v SoftReset | grep -v ASE | grep -v HW | grep -v UMSG | grep -v Allocated | grep -v Workspace | grep -v Host  | less > $ASEVAL_GIT/random_test_log_$ii.txt
     cp $ASE_WORKDIR/ccip_transactions.tsv $ASEVAL_GIT/transactions_$ii.txt
 done
+
+diff -q $ASEVAL_GIT/random_test_log_1.txt $ASEVAL_GIT/random_test_log_2.txt
+
--- test_afus/ccip_nlb_all/SW/fpgadiag_include.sh
+++ test_afus/ccip_nlb_all/SW/fpgadiag_include.sh
@@ -4,6 +4,6 @@
 fpgadiag_rdvc_arr="--rva --rvl0 --rvh0 --rvh1 --rvr"
 fpgadiag_wrvc_arr="--wva --wvl0 --wvh0 --wvh1 --wvr"
 fpgadiag_mcl_arr="1 2 4"
-fpgadiag_rdtype_arr="--rds --rdi"
+fpgadiag_rdtype_arr="--rdi"
 fpgadiag_wrtype_arr="--wlm --wli --wpi"
 
