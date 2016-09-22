0bb5a6b83b6edd8f96e203a285f9f3712076f55a
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-09-22 15:21:00 -0700

    Added klocwork scripted run

---
+++ klocwork_run.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+export PATH=/home/rrsharma/kw10/bin:$PATH
+
+export KW_BUILD=$AALUSER_DIR/kw_build/
+
+export PATH=/home/rrsharma/kw10/bin:${PATH}
+
+# delete and rebuild KWbuild
+rm -rf $KW_BUILD
+mkdir -p $KW_BUILD
+
+## configure
+cd $KW_BUILD
+../configure
+
+kwcheck create --url https://klocwork-jf3.devtools.intel.com:8085/AALUSER
+
+kwshell --verbose make -j 8
+
+kwcheck run
+kwcheck list -F detailed --local --system --severity 1 > $ASEVAL_GIT/kw_run.1.log
+kwcheck list -F detailed --local --system --severity 2 > $ASEVAL_GIT/kw_run.2.log
+
