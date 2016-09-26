d47361fdb987a89826165b02ab0da7e620002aa9
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-09-26 15:15:00 -0700

    klocwork_run.sh : Added ase build to klocwork scripted run

--- klocwork_run.sh
+++ klocwork_run.sh
@@ -22,3 +22,19 @@ kwcheck run
 kwcheck list -F detailed --local --system --severity 1 > $ASEVAL_GIT/kw_run.1.log
 kwcheck list -F detailed --local --system --severity 2 > $ASEVAL_GIT/kw_run.2.log
 
+############################################################################
+
+source /opt/vcs.sh
+
+source /opt/quartus_16.sh
+
+cd $ASE_SRCDIR
+
+kwcheck create --url https://klocwork-jf3.devtools.intel.com:8085/AALUSER
+
+kwshell --verbose make
+
+kwcheck run
+kwcheck list -F detailed --local --system --severity 1 > $ASEVAL_GIT/kw_ase.1.log
+kwcheck list -F detailed --local --system --severity 2 > $ASEVAL_GIT/kw_ase.2.log
+
