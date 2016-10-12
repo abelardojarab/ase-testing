9a44e3248a22b9363d0bf61bf82c7130f9b489d7
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-10-12 09:33:36 -0700

    mmio_stress_afu is synthesizable, now uses a RAM to build MMIO space Tested in ASE, to be built using SKX-P package

--- test_afus/ccip_mmio_rdwr_stress/HW/mmio_stress_afu.sv
+++ test_afus/ccip_mmio_rdwr_stress/HW/mmio_stress_afu.sv
@@ -15,70 +15,255 @@ module mmio_stress_afu
    output 	     t_if_ccip_Tx pck_af2cp_sTx
    );
 
-   parameter int     CCIP_MMIO_BYTEWIDTH = CCIP_MMIOADDR_WIDTH + 2;
+
+// `define MANIFEST_0  64'h1000000000001071
+// `define AFUID_LO    64'h96bf6f5fc4038fac
+// `define AFUID_HI    64'h10c1bff188d14dfb
+
+   logic [63:0]      afu_dfh [0:2];
+
+   assign afu_dfh[0] = 64'h1000000000001071;
+   assign afu_dfh[1] = 64'h96bf6f5fc4038fac;
+   assign afu_dfh[2] = 64'h10c1bff188d14dfb;
+
+   // Temp singal
+   logic [14:0]      ram_addr;
+   logic [7:0] 	     ram_byte_en;
+   logic [63:0]      ram_data_in;
+   logic [63:0]      ram_data_out;
+   logic 	     ram_write_en;
+   logic 	     ram_read_en;
+   logic 	     ram_data_valid;
+
+   logic [8:0] 	     ram_tid_out;
+   logic [1:0] 	     ram_read_type;
+   logic 	     ram_mask_loc;
    
-   (* ramstyle = "M20K" *) logic [7:0] 	     regs [0:2**CCIP_MMIO_BYTEWIDTH-1];
-   // logic [7:0] 	     regs [0:CCIP_MMIO_BYTEWIDTH-1];
+   logic [63:0]      ram_data_masked;
+   logic 	     ram_data_masked_valid;
+         
+   logic 	     clk;
+   assign clk = pClk;
 
+   // Instantiate RAM_1R1W
+   ram_1r1w ram_1r1w
+     (
+      .address (ram_addr),
+      .byteena (ram_byte_en),
+      .clock   (clk),
+      .data    (ram_data_in),
+      .wren    (ram_write_en),
+      .q       (ram_data_out)
+      );
 
-`define AFUID_LO   64'h96bf6f5fc4038fac
-`define AFUID_HI   64'h10c1bff188d14dfb
-
+   // CCIP memory header & mapping
    t_ccip_c0_ReqMmioHdr CfgHdr;
-   logic [CCIP_MMIO_BYTEWIDTH-1:0] cfg_mmioaddr;
-   
    assign CfgHdr = t_ccip_c0_ReqMmioHdr'(pck_cp2af_sRx.c0.hdr);
-   assign cfg_mmioaddr = {CfgHdr.address, 2'b0};
+
+   // logic [8:0] 	     mmio_rtid;
    
    
-   // MMIO Write
-   always @(posedge pClk) begin
-      if (pck_cp2af_softReset) begin
-	 {regs[7],regs[6],regs[5],regs[4],regs[3],regs[2],regs[1],regs[0]}       <= 64'h1000000000001071;
-	 {regs[15],regs[14],regs[13],regs[12],regs[11],regs[10],regs[9],regs[8]} <= `AFUID_LO;
-	 {regs[23],regs[22],regs[21],regs[20],regs[19],regs[18],regs[17],regs[16]} <= `AFUID_HI;	 
+   // logic [63:0]      mmio_rdata_0;
+   // logic [8:0] 	     mmio_rtid_0;
+   // logic 	     mmio_rvalid_0;
+
+   // logic [63:0]      mmio_rdata_1;
+   // logic [8:0] 	     mmio_rtid_1;
+   // logic 	     mmio_rvalid_1;
+
+
+   /*
+    * Soft reset, starts off a 4-cycle long internal reset
+    */
+   // Reset status
+   logic 	     softreset_q;
+   logic 	     softreset;
+   logic 	     internal_rst;
+   logic 	     rst_trigger;
+   int 		     rst_counter;
+
+   typedef enum {
+		 ResetIdle,
+		 ResetLine1,
+		 ResetLine2,
+		 ResetLine3,
+		 ResetDone
+		 } ResetStateEnum;
+   ResetStateEnum rst_state = ResetIdle;
+
+   assign softreset = pck_cp2af_softReset;
+
+   always @(posedge clk) begin
+      softreset_q <= softreset;
+   end
+
+   always @(posedge clk) begin
+      if (softreset & ~softreset_q) begin
+	 rst_trigger <= 1;
       end
       else begin
-	 if (pck_cp2af_sRx.c0.mmioWrValid && (CfgHdr.length == 2'b00)) begin	 
-	    regs [cfg_mmioaddr + 0] <= pck_cp2af_sRx.c0.data[ 7:0];
-	    regs [cfg_mmioaddr + 1] <= pck_cp2af_sRx.c0.data[15:8];
-	    regs [cfg_mmioaddr + 2] <= pck_cp2af_sRx.c0.data[23:16];
-	    regs [cfg_mmioaddr + 3] <= pck_cp2af_sRx.c0.data[31:24];	 
-	 end
-	 else if (pck_cp2af_sRx.c0.mmioWrValid && (CfgHdr.length == 2'b01)) begin
-	    regs [cfg_mmioaddr + 0] <= pck_cp2af_sRx.c0.data[ 7:0 ];
-	    regs [cfg_mmioaddr + 1] <= pck_cp2af_sRx.c0.data[15:8 ];
-	    regs [cfg_mmioaddr + 2] <= pck_cp2af_sRx.c0.data[23:16];
-	    regs [cfg_mmioaddr + 3] <= pck_cp2af_sRx.c0.data[31:24];
-	    regs [cfg_mmioaddr + 4] <= pck_cp2af_sRx.c0.data[39:32];
-	    regs [cfg_mmioaddr + 5] <= pck_cp2af_sRx.c0.data[47:40];
-	    regs [cfg_mmioaddr + 6] <= pck_cp2af_sRx.c0.data[55:48];
-	    regs [cfg_mmioaddr + 7] <= pck_cp2af_sRx.c0.data[63:56];
-	 end
+	 rst_trigger <= 0;
       end
    end
 
-   // MMIO Read
-   always @(posedge pClk) begin
-      if (pck_cp2af_sRx.c0.mmioRdValid && (CfgHdr.length == 2'b00)) begin
-	 pck_af2cp_sTx.c2.data        <= {32'b0, regs [cfg_mmioaddr + 3], regs [cfg_mmioaddr + 2], regs [cfg_mmioaddr + 1], regs [cfg_mmioaddr + 0]};	    
-	 pck_af2cp_sTx.c2.hdr.tid     <= CfgHdr.tid;
-	 pck_af2cp_sTx.c2.mmioRdValid <= 1;
-      end
-      else if (pck_cp2af_sRx.c0.mmioRdValid && (CfgHdr.length == 2'b01)) begin
-	 pck_af2cp_sTx.c2.data        <= {regs [cfg_mmioaddr+7], regs [cfg_mmioaddr+6], regs [cfg_mmioaddr+5], regs [cfg_mmioaddr+4], regs [cfg_mmioaddr+3], regs [cfg_mmioaddr+2], regs [cfg_mmioaddr+1], regs [cfg_mmioaddr+0]};
-	 pck_af2cp_sTx.c2.hdr.tid     <= CfgHdr.tid;
-	 pck_af2cp_sTx.c2.mmioRdValid <= 1;
+   /*
+    * Main process =>
+    * Reset initializer FSM
+    * Read/Write process
+    */
+   always @(posedge clk) begin
+      // -------------------------------------------- //
+      // Reset Initializer
+      // -------------------------------------------- //
+      if (softreset) begin
+	 case (rst_state)
+   	   ResetIdle:
+   	     begin
+   		ram_write_en <= 0;
+   		if (rst_trigger) begin
+   		   internal_rst <= 1;
+   		   rst_state <= ResetLine1;
+   		end
+   		else begin
+   		   internal_rst <= 0;
+   		   rst_state <= ResetIdle;
+   		end
+   	     end
+
+   	   ResetLine1:
+   	     begin
+   		internal_rst <= 1;
+   		rst_state <= ResetLine2;
+   		ram_write_en <= 1;
+   		ram_data_in  <= afu_dfh[0];
+		ram_byte_en  <= 8'hFF;
+   		ram_addr     <= 14'h000;
+   	     end
+
+   	   ResetLine2:
+   	     begin
+   		internal_rst <= 1;
+   		rst_state <= ResetLine3;
+   		ram_write_en <= 1;
+   		ram_data_in  <= afu_dfh[1];
+		ram_byte_en  <= 8'hFF;
+   		ram_addr     <= 14'h001;
+   	     end
+
+   	   ResetLine3:
+   	     begin
+   		internal_rst <= 1;
+   		rst_state <= ResetDone;
+   		ram_write_en <= 1;
+   		ram_data_in  <= afu_dfh[2];
+		ram_byte_en  <= 8'hFF;
+   		ram_addr     <= 14'h002;
+   	     end
+
+   	   ResetDone:
+   	     begin
+   		internal_rst <= 1;
+   		rst_state <= ResetIdle;
+   		ram_write_en <= 0;
+		ram_byte_en  <= 8'h00;
+   		ram_addr     <= 14'h000;
+   		ram_data_in  <= 64'b0;
+   	     end
+
+   	   default:
+   	     begin
+   		internal_rst <= 0;
+   		rst_state <= ResetIdle;
+   		ram_write_en <= 0;
+   	     end
+	 endcase
       end
       else begin
-	 pck_af2cp_sTx.c2.mmioRdValid <= 0;
+	 // ------------------------------------------------ //
+	 // MMIO Write process (start to end)
+	 // ------------------------------------------------ //
+	 ram_write_en  <= 0;
+	 ram_read_en   <= 0;	 
+	 if (pck_cp2af_sRx.c0.mmioWrValid) begin
+	    ram_write_en <= 1;
+	    // 32-bit write
+	    if (CfgHdr.length == 2'b00) begin
+	       ram_addr    <= CfgHdr.address[15:1];
+	       if (CfgHdr.address[0] == 0) begin
+		  ram_byte_en <= 8'h0F;
+		  ram_data_in <= {32'b0, pck_cp2af_sRx.c0.data[31:0]};
+	       end
+	       else if (CfgHdr.address[0] == 1) begin
+		  ram_byte_en <= 8'hF0;
+		  ram_data_in <= {pck_cp2af_sRx.c0.data[31:0], 32'b0};
+	       end
+	    end
+	    // 64-bit write
+	    else if (CfgHdr.length == 2'b01) begin
+	       ram_addr    <= CfgHdr.address[15:1];
+	       ram_byte_en <= 8'hFF;
+	       ram_data_in <= pck_cp2af_sRx.c0.data[63:0];
+	    end
+	 end
+	 // ------------------------------------------------ //
+	 // MMIO Read process (start)
+	 // ------------------------------------------------ //
+	 else if (pck_cp2af_sRx.c0.mmioRdValid) begin
+	    ram_write_en <= 0;
+	    ram_byte_en  <= 8'hFF;
+	    ram_addr     <= CfgHdr.address[15:1];
+	    ram_tid_out  <= CfgHdr.tid;
+	    ram_read_type <= CfgHdr.length;
+	    ram_mask_loc  <= CfgHdr.address[0];
+	    ram_read_en  <= 1;		  
+	 end	 
       end
    end
+
+   // Read Valid indicator
+   always @(posedge clk) begin
+      ram_data_valid <= ram_read_en;      
+   end
+
+   // Masked data
+   always @(posedge clk) begin
+      if (ram_data_valid) begin
+	 ram_data_masked_valid <= 1;
+	 // 32 vs 64
+	 if (ram_read_type == 2'b00) begin
+	    if (ram_mask_loc == 0) begin
+	       ram_data_masked <= {32'b0, ram_data_out[31:0]};
+	    end
+	    else if (ram_mask_loc == 1) begin
+	       ram_data_masked <= {32'b0, ram_data_out[63:32]};
+	    end
+	 end
+	 else if (ram_read_type == 2'b01) begin
+	    ram_data_masked <= ram_data_out;
+	 end
+      end
+      else begin
+	 ram_data_masked_valid <= 0;	 
+      end
+   end
+  
+   
+   // MMIO Read Response (end)
+   always @(posedge clk) begin
+      if (softreset) begin
+   	 pck_af2cp_sTx.c2.mmioRdValid <= 0;
+      end
+      else begin
+   	 pck_af2cp_sTx.c2.hdr.tid     <= ram_tid_out;	 
+   	 pck_af2cp_sTx.c2.data        <= ram_data_masked;	 
+   	 pck_af2cp_sTx.c2.mmioRdValid <= ram_data_masked_valid;	 
+      end
+   end
+
 
    // TX channel 0 out
    assign pck_af2cp_sTx.c0 = 0;
    assign pck_af2cp_sTx.c1 = 0;
-   
-   
-endmodule // mmio_stress_afu
 
+
+endmodule // mmio_stress_afu
