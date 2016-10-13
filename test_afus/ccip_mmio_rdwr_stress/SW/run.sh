666f5a24db0977503f4bb5cd3e2d1a6608272de0
Rahul R Sharma <rahul.r.sharma@intel.com>
2016-10-13 10:56:11 -0700

    Added back ccip_mmio_stress AFU without ASYNC, design still uses RAM

---
+++ test_afus/ccip_mmio_rdwr_stress/SW/run.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+make prefix=$MYINST_DIR
+
+# Wait for readiness
+echo "##################################"
+echo "#     Waiting for .ase_ready     #"
+echo "##################################"
+while [ ! -f $ASE_WORKDIR/.ase_ready.pid ]
+do
+    sleep 1
+done
+
+# Simulator PID
+ase_pid=`cat $ASE_WORKDIR/.ase_ready.pid | grep pid | cut -d "=" -s -f2-`
+
+/usr/bin/timeout 3600 ./mmio_stress
+errcode=$?
+if [[ $errcode != 0 ]]
+then
+    echo "** mmio_stress: FAILURE EXIT !! Error code $errcode **"
+    exit 1
+fi
+
+
