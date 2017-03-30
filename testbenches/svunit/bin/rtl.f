9b78f6b1bd26c4cd9b0997bbdcfc9e53926995c6
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-03-30 15:24:45 -0700

    Delisted generated files, rtl.f updates to +define+STANDALONE_DEBUG +incdir, -full64 added to Makefile

--- testbenches/svunit/bin/rtl.f
+++ testbenches/svunit/bin/rtl.f
@@ -1,5 +1,6 @@
-++incdir+./$ASE_WORKDIR
-$ASE_WORKDIR/rtl/ccip_if_pkg.sv
-$ASE_WORKDIR/rtl/ase_pkg.sv
-$ASE_WORKDIR/rtl/ccip_checker.sv
++incdir+$ASE_SRCDIR/rtl/
++define+STANDALONE_DEBUG
+$ASE_SRCDIR/rtl/ccip_if_pkg.sv
+$ASE_SRCDIR/rtl/ase_pkg.sv
+$ASE_SRCDIR/rtl/ccip_checker.sv
 
