986036e99a0d22757880882388161757497825ce
Sherlekar, Dipti <dipti.sherlekar@intel.com>
2017-04-10 11:52:37 -0700

    Fix Jenkins Script to support old ASE scripts.

--- Jenkins_scripts/Jenkins_script.sh
+++ Jenkins_scripts/Jenkins_script.sh
@@ -1,37 +1,21 @@
 #!/bin/sh
-SRCDIR=$PWD
 
-ASE_SRCDIR=$SRCDIR/cpt_sys_sw-fpga-sw/ase
-cd cpt_sys_sw-fpga-sw/ase
+cd $ASE_SRCDIR
 make clean
-##`rm -rf ase_regression-validation
-##rm -rf cpt_sys_sw-fpga-sw
-##rm -rf mybuild
 
-##cd $SRCDIR
-## ASE_Regression
-##git clone ssh://diptishe@git-amr-3.devtools.intel.com:29418/ase_regression-validation `
+rm -rf $ASE_API_DIR/mybuild
 
-cd $SRCDIR/ase_regression-validation
-./env.sh $SRCDIR
 echo '************************************'
 echo $ASEVAL_GIT
 echo '************************************'
-#AFU=$SRCDIR/ase_regression-validation/test_afus/ccip_nlb_all_SKX1/HW
-AFU=$SRCDIR/ase_regression-validation/test_afus/ccip_mmio_rdwr_stress/HW
-echo $AFU
-##`git clone ssh://diptishe@git-amr-1.devtools.intel.com:29418/cpt_sys_sw-fpga-sw `
-##cd $FPGASW_GIT
-##git checkout develop
+echo $1
 
-
-## BBB
-##cd $SRCDIR
-##git clone ssh://diptishe@git-amr-1.devtools.intel.com:29418/atd_fpga_app-qa_bblocks 
+echo '********************************************************************************'
+echo '******************** Setting up the ASE Environment ****************************'
+echo '********************************************************************************'
 
 cd $ASE_SRCDIR
-python scripts/generate_ase_environment.py $AFU
-##python scripts/generate_ase_environment.py $AFU
+python scripts/generate_ase_environment.py $1
 
 make -j8
 make sim &
@@ -40,11 +24,11 @@ make sim &
 ##################            APPLICATION  SIDE            ####################
 ###############################################################################
 
-echo " #################################################"
-echo " #   	 Build libfpga-ASE library             #"
-echo " #################################################"
-rm -rf $ASE_SRCDIR/api/mybuild
-cd $ASE_SRCDIR/api
+echo "############################### #################################################"
+echo "######################   	 Build libfpga-ASE library    #########################"
+echo "#################################################################################"
+rm -rf $ASE_API_DIR/mybuild
+cd $ASE_API_DIR
 mkdir  mybuild
 cd mybuild
 
@@ -54,27 +38,24 @@ make VERBOSE=1
 
 export LD_LIBRARY_PATH=$PWD
 
-echo "########################################"
-echo "#            Building samples          #"
-echo "########################################"
-
+echo "############################### #################################################"
+echo "#############################   	 Building samples   # #########################"
+echo "#################################################################################"
 LIBNAME=libfpga-ASE.so
-#cd $SRCDIR
-## Build sample
-#cd $SRCDIR
-#gcc -g -o hello_fpga $SRCDIR/cpt_sys_sw-fpga-sw/fpga-api/samples/hello_fpga.c $LIBNAME -I$SRCDIR/cpt_sys_sw-fpga-sw/common/include/ -std=c99 -luuid -lgcov
-gcc -g -o mmio_stress $SRCDIR/ase_regression-validation/test_afus/ccip_mmio_rdwr_stress/SW/mmio_stress.c $LIBNAME -I$SRCDIR/cpt_sys_sw-fpga-sw/common/include/ -std=c99 -luuid -lgcov
-export ASE_WORKDIR=$ASE_SRCDIR/work
-#cd $SRCDIR
+
+gcc -g -o $TEST_AFU_DIR $1/SW/*.c $LIBNAME -I$AALSDK_GIT/common/include/ -std=c99 -luuid -lgcov
+export ASE_WORKDIR=$ASE_WORKDIR
+if [ -z "$ASE_WORKDIR" ]
+then
+echo '??????????????????????????????? ASE_WORKDIR is not set ??????????????????????????'
+else
+echo "#############################   ASE_WORKDIR is set   ############################"
 lcov --zerocounters --directory .
-lcov --capture --initial --directory . --output-file app_mmio
+lcov --capture --initial --directory . --output-file coverage_new
 
-./mmio_stress
+./$TEST_AFU_DIR
 
-#if[$? -eq 0];  then
+lcov --no-checksum --directory .  --capture --output-file coverage_new.info
+genhtml coverage_new.info --output-directory coverage_out
 
-lcov --no-checksum --directory .  --capture --output-file app_mmio.info
-genhtml app_mmio.info --output-directory out_mmio
-#else
-#	echo "FAILED EXECUTION"
-#fi
+fi
