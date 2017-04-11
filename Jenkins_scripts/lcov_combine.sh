db5ee5c8b14985500addc268541d7198d777c2f5
Sherlekar, Dipti <dipti.sherlekar@intel.com>
2017-04-11 12:02:06 -0700

    Changes for Jenkins task - Total Lcov coverage.

--- Jenkins_scripts/lcov_combine.sh
+++ Jenkins_scripts/lcov_combine.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-lcov -a SCRIPT/cpt_sys_sw-fpga-sw/ase/api/mybuild/app_mmio.info -a SCRIPT_NLB3/cpt_sys_sw-fpga-sw/ase/api/mybuild/app.info -o total.info  # combine them into total.info
+lcov -a /home/lab/workspace/jenkins/workspace/ASE_Coverage/TEST_AFU_DIR/ccip_nlb_all_SKX1/label/ASE/$AALSDK_GIT/ase/api/mybuild/*.info -a /home/lab/workspace/jenkins/workspace/ASE_Coverage/TEST_AFU_DIR/ccip_mmio_rdwr_stress/label/ASE/$AALSDK_GIT/ase/api/mybuild/*.info -o total.info  # combine them into total.info
 genhtml total.info --output-directory total_info
