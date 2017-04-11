011971fa8ff472872f08038cfc3670ad16185daa
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-04-11 13:23:47 -0700

    Code cleanup

--- klocwork_run.sh
+++ klocwork_run.sh
@@ -20,34 +20,3 @@ kwcheck list -F detailed --local --system --severity 2 > $ASEVAL_GIT/kw_run.2.lo
 kwcheck list -F detailed --local --system --severity 3 > $ASEVAL_GIT/kw_run.3.log
 kwcheck list -F detailed --local --system --severity 4 > $ASEVAL_GIT/kw_run.4.log
 kwcheck list -F detailed --local --system --severity 5 > $ASEVAL_GIT/kw_run.5.log
-
-# ## configure
-# cd $KW_BUILD
-# cmake $ASE_SRCDIR/api/
-
-# ## Klocwork
-# rm -rf $ASEVAL_GIT/kw_run.*
-# kwcheck create --url https://klocwork-jf3.devtools.intel.com:8085/AALUSER
-# kwshell --verbose make -j 8
-# kwcheck run
-# kwcheck list -F detailed --local --system --severity 1 > $ASEVAL_GIT/kw_run.1.log
-# kwcheck list -F detailed --local --system --severity 2 > $ASEVAL_GIT/kw_run.2.log
-# kwcheck list -F detailed --local --system --severity 3 > $ASEVAL_GIT/kw_run.3.log
-# kwcheck list -F detailed --local --system --severity 4 > $ASEVAL_GIT/kw_run.4.log
-# kwcheck list -F detailed --local --system --severity 5 > $ASEVAL_GIT/kw_run.5.log
-
-# ############################################################################
-# cd $ASE_SRCDIR
-# rm -rf $ASEVAL_GIT/kw_ase.*
-# kwcheck create --url https://klocwork-jf3.devtools.intel.com:8085/AALUSER
-# kwshell --verbose make sw_build
-# kwcheck run
-# kwcheck list -F detailed --local --system --severity 1 > $ASEVAL_GIT/kw_ase.1.log
-# kwcheck list -F detailed --local --system --severity 2 > $ASEVAL_GIT/kw_ase.2.log
-# kwcheck list -F detailed --local --system --severity 3 > $ASEVAL_GIT/kw_ase.3.log
-# kwcheck list -F detailed --local --system --severity 4 > $ASEVAL_GIT/kw_ase.4.log
-# kwcheck list -F detailed --local --system --severity 5 > $ASEVAL_GIT/kw_ase.5.log
-
-
-# cd $ASEVAL_GIT/
-# ls -lt kw_*.log
