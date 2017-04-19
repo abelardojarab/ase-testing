f6020b7f4de97f1b4509a542a3dffbe55c6d7b29
Sherlekar, Dipti <dipti.sherlekar@intel.com>
2017-04-19 14:32:20 -0700

    Changes to Jenkins_script to support NLB_MUX_4

--- Jenkins_scripts/Jenkins_script.sh
+++ Jenkins_scripts/Jenkins_script.sh
@@ -17,12 +17,13 @@ cd $ASE_SRCDIR
 if [ "$TEST_AFU_DIR" = 'ccip_async_mux_4nlb' ]; then
 #cp $ASEVAL_GIT/test_afus/$TEST_AFU_DIR/config/SKX1/* ./ 
 cp $BBB_GIT/BBB_ccip_mux/sample/sw/*.c $ASEVAL_GIT/test_afus/$TEST_AFU_DIR/SW
-#sed -i 's/define+NLB400_MODE_0/define+NLB400_MODE_0 +define+NUM_AFUS_4/g' Makefile
+sed -i 's/define+NLB400_MODE_0/define+NLB400_MODE_0 +define+NUM_AFUS_4/g' Makefile
 elif [ "$TEST_AFU_DIR" = 'ccip_mmio_rdwr_stress' ]; then
 #cp $ASEVAL_GIT/test_afus/$TEST_AFU_DIR/config/SKX1/* ./ 
 echo " DO NOTHING"
 else
 cp $AALSDK_GIT/libfpga/samples/hello_fpga.c $ASEVAL_GIT/test_afus/$TEST_AFU_DIR/SW
+#echo "DO NOTHING"
 fi
 
 #rm -rf $ASE_WORKDIR/.ase_ready.pid
