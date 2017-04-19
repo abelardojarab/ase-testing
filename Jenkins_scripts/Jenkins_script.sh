36fa971ab2140300568bf0d3570583f3d059f57a
Sherlekar, Dipti <dipti.sherlekar@intel.com>
2017-04-19 12:23:57 -0700

    Changes to support MMIO_STRESS TESTS in Jenkins script.

--- Jenkins_scripts/Jenkins_script.sh
+++ Jenkins_scripts/Jenkins_script.sh
@@ -18,9 +18,8 @@ if [ "$TEST_AFU_DIR" = 'ccip_async_mux_4nlb' ]; then
 #cp $ASEVAL_GIT/test_afus/$TEST_AFU_DIR/config/SKX1/* ./ 
 cp $BBB_GIT/BBB_ccip_mux/sample/sw/*.c $ASEVAL_GIT/test_afus/$TEST_AFU_DIR/SW
 #sed -i 's/define+NLB400_MODE_0/define+NLB400_MODE_0 +define+NUM_AFUS_4/g' Makefile
-elif [ "$TEST_AFU_DIR" = 'ccip_async_nlb300_all' ] || [ "$TEST_AFU_DIR" = 'ccip_async_nlb100_all' ]; then
+elif [ "$TEST_AFU_DIR" = 'ccip_mmio_rdwr_stress' ]; then
 #cp $ASEVAL_GIT/test_afus/$TEST_AFU_DIR/config/SKX1/* ./ 
-cp $AALSDK_GIT/libfpga/samples/hello_fpga.c $ASEVAL_GIT/test_afus/$TEST_AFU_DIR/SW
 else
 cp $AALSDK_GIT/libfpga/samples/hello_fpga.c $ASEVAL_GIT/test_afus/$TEST_AFU_DIR/SW
 fi
