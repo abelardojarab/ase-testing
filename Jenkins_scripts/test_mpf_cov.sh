73b390d0e2beba8be6d572cc1a02964cf2aff116
Sherlekar, Dipti <dipti.sherlekar@intel.com>
2017-04-20 11:27:53 -0700

    Changes to test_mpf_cov to support Coverage for MPF.

--- Jenkins_scripts/test_mpf_cov.sh
+++ Jenkins_scripts/test_mpf_cov.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-cd $MY_INSTDIR
+cd $ASE_SRCDIR/api/mybuild
 lcov --zerocounters --directory .
 lcov --capture --initial --directory . --output-file coverage_new
 cd $BBB_GIT/BBB_cci_mpf/test/test-mpf/test_random/sw/
 ./$1 --target=ase --tc=100
 
-lcov -capture --directory $MY_INSTDIR ./ase/api/CMakeFiles/fpga-ASE.dir/src/ -o coverage.info
+cd $ASE_SRCDIR/api/mybuild
+lcov -capture --directory . ./ase/api/CMakeFiles/fpga-ASE.dir/src/ -o coverage.info
 genhtml coverage.info --output-directory coverage_out
