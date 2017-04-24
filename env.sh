fb60937f108617f5bd5be21c4c1ca83d60d71909
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-04-24 16:22:27 -0700

    Add fpga-internal repo into update_git and env scripts

--- env.sh
+++ env.sh
@@ -11,7 +11,7 @@ export ASEVAL_GIT=$BASEDIR/ase_regression/
 export BBB_GIT=$BASEDIR/BuildingBlocks/
 export BDX_GIT=$BASEDIR/bdx_fpga_piu/
 export FPGASW_GIT=$BASEDIR/fpga-sw/
-
+export FPGAINT_GIT=$BASEDIR/cpt_sys_sw-fpga-internal/
 export ASE_SRCDIR=$FPGASW_GIT/ase
 export ASE_WORKDIR=$FPGASW_GIT/ase/work
 export MYINST_DIR=$BASEDIR/myinst
@@ -20,10 +20,10 @@ export RELCODE="SKX1"
 
 echo "Directory settings =>"
 echo "FPGASW_GIT  : " $FPGASW_GIT
+echo "FPGAINT_GIT : " $FPGAINT_GIT
 echo "ASEVAL_GIT  : " $ASEVAL_GIT
 echo "BBB_GIT     : " $BBB_GIT
 echo "ASE_SRCDIR  : " $ASE_SRCDIR
 echo "ASE_WORKDIR : " $ASE_WORKDIR
 echo "MYINST_DIR  : " $MYINST_DIR
 echo "RELCODE     : " $RELCODE
-
