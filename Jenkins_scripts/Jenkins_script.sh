7be9edfd3e0e6aff3a2f43cda6c6d36938670f40
Sherlekar, Dipti <dipti.sherlekar@intel.com>
2017-04-24 17:36:02 -0700

    Changes to script for Coverage.

--- Jenkins_scripts/Jenkins_script.sh
+++ Jenkins_scripts/Jenkins_script.sh
@@ -75,7 +75,7 @@ lcov --capture --initial --directory . --output-file coverage_new
 
 ./$TEST_AFU_DIR
 
-lcov -capture --directory ./ase/api/CMakeFiles/fpga-ASE.dir/ -o coverage.info
-genhtml coverage.info --output-directory coverage_out
+lcov -capture --directory ./ase/api/CMakeFiles/fpga-ASE.dir/ -o ${TEST_AFU_DIR}.info
+genhtml ${TEST_AFU_DIR}.info --output-directory coverage 
 fi
 fi
