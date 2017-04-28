fece25662f75a0233420b97ca96ee034f7367e98
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-04-27 22:39:20 -0700

    apps peek-poke script mod - removed setup for /home/rrsharma/xeon-fpga-src/fpga-sw//ase

--- apps/build_all.sh
+++ apps/build_all.sh
@@ -12,44 +12,25 @@ rm -rf *.out output.*.log
 # fi
 
 set -e
+set -v
 
-gcc $COV_FLAGS -g -o nlb_test.out \
-    nlb_lpbk1_test.c \
-    ${ASE_SRCDIR}/sw/tstamp_ops.c \
-    ${ASE_SRCDIR}/sw/ase_ops.c \
-    ${ASE_SRCDIR}/sw/app_backend.c \
-    ${ASE_SRCDIR}/sw/mqueue_ops.c \
-    ${ASE_SRCDIR}/sw/error_report.c \
+gcc $COV_FLAGS -fPIC -g -o nlb_test.out \
+    nlb_lpbk1_test.c ${MYINST_DIR}/lib/libfpga-ASE.so \
     -lrt -lm -lpthread -I ${ASE_SRCDIR}/sw/ \
 #    -D ASE_DEBUG
 
 gcc -g -o mmio_test.out \
-    mmio_test.c \
-    ${ASE_SRCDIR}/sw/tstamp_ops.c \
-    ${ASE_SRCDIR}/sw/ase_ops.c \
-    ${ASE_SRCDIR}/sw/app_backend.c \
-    ${ASE_SRCDIR}/sw/mqueue_ops.c \
-    ${ASE_SRCDIR}/sw/error_report.c \
+    mmio_test.c ${MYINST_DIR}/lib/libfpga-ASE.so \
     -lrt -lm -lpthread -I ${ASE_SRCDIR}/sw/ \
 #    -D ASE_DEBUG
 
 gcc  -g -o alloc_dealloc.out \
-    alloc_dealloc.c \
-    ${ASE_SRCDIR}/sw/tstamp_ops.c \
-    ${ASE_SRCDIR}/sw/ase_ops.c \
-    ${ASE_SRCDIR}/sw/app_backend.c \
-    ${ASE_SRCDIR}/sw/mqueue_ops.c \
-    ${ASE_SRCDIR}/sw/error_report.c \
+    alloc_dealloc.c ${MYINST_DIR}/lib/libfpga-ASE.so \
     -lrt -lm -lpthread -I ${ASE_SRCDIR}/sw/ \
 #    -D ASE_DEBUG
 
 gcc  -g -o alloc_stress_test.out \
-    alloc_stress_test.c \
-    ${ASE_SRCDIR}/sw/tstamp_ops.c \
-    ${ASE_SRCDIR}/sw/ase_ops.c \
-    ${ASE_SRCDIR}/sw/app_backend.c \
-    ${ASE_SRCDIR}/sw/mqueue_ops.c \
-    ${ASE_SRCDIR}/sw/error_report.c \
+    alloc_stress_test.c ${MYINST_DIR}/lib/libfpga-ASE.so \
     -lrt -lm -lpthread -I ${ASE_SRCDIR}/sw/ \
 
 # gcc -g -o mux_nlb_test.out \
@@ -63,21 +44,11 @@ gcc  -g -o alloc_stress_test.out \
 #     -D ASE_DEBUG
 
 gcc  -g -o session_stress.out \
-    session_stress.c \
-    ${ASE_SRCDIR}/sw/tstamp_ops.c \
-    ${ASE_SRCDIR}/sw/ase_ops.c \
-    ${ASE_SRCDIR}/sw/app_backend.c \
-    ${ASE_SRCDIR}/sw/mqueue_ops.c \
-    ${ASE_SRCDIR}/sw/error_report.c \
+    session_stress.c ${MYINST_DIR}/lib/libfpga-ASE.so \
     -lrt -lm -lpthread -I ${ASE_SRCDIR}/sw/ \
 
 gcc  -g -o umsg_test.out \
-    umsg_test.c \
-    ${ASE_SRCDIR}/sw/tstamp_ops.c \
-    ${ASE_SRCDIR}/sw/ase_ops.c \
-    ${ASE_SRCDIR}/sw/app_backend.c \
-    ${ASE_SRCDIR}/sw/mqueue_ops.c \
-    ${ASE_SRCDIR}/sw/error_report.c \
+    umsg_test.c ${MYINST_DIR}/lib/libfpga-ASE.so \
     -lrt -lm -lpthread -I ${ASE_SRCDIR}/sw/ \
 
 
