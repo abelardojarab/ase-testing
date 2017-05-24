73523f5abcef50ec97f98e52c5e3ccdeaac80a4e
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-05-24 15:57:15 -0700

    Modified ccip_nlb_mode0 to remove custom makefile, changes added to ase_sources.mk

--- test_afus/ccip_nlb_mode0/config/SKX1/ase_sources.mk
+++ test_afus/ccip_nlb_mode0/config/SKX1/ase_sources.mk
@@ -9,8 +9,6 @@ DUT_VLOG_SRC_LIST = $(ASE_SRCDIR)/vlog_files.list
 
 DUT_INCDIR = $(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/include_files+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/include_files/common+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/lpbk1_RdRspRAM2PORT+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/lpbk1_RdRspRAM2PORT/synth+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/lpbk1_RdRspRAM2PORT/ram_2port_160+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/lpbk1_RdRspRAM2PORT/ram_2port_160/synth+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/req_C1TxRAM2PORT+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/req_C1TxRAM2PORT/synth+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/req_C1TxRAM2PORT/ram_2port_160+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/req_C1TxRAM2PORT/ram_2port_160/synth+
 
-ASE_TOP = ase_top
-
 SIMULATOR ?= VCS
 
 ASE_PLATFORM = ASE_PLATFORM_MCP_SKYLAKE
