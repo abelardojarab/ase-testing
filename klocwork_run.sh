6da838e854784ab2f3e39e299ff75b7b56865758
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-07-06 20:58:59 -0700

    Mod to klocwork script

--- klocwork_run.sh
+++ klocwork_run.sh
@@ -12,9 +12,11 @@ mkdir -p $KW_BUILD
 cd $KW_BUILD
 
 rm -rf $ASEVAL_GIT/kw_run.*
-# kwcheck create --url https://klocwork-jf3.devtools.intel.com:8085/FPGA_API_ASE
-kwcheck create --url https://klocwork-jf3.devtools.intel.com:8085/BBB
-kwinject $ASEVAL_GIT/sw_build_all.sh
+kwcheck create --url https://klocwork-jf3.devtools.intel.com:8085/FPGA_API_ASE
+# kwcheck create --url https://klocwork-jf3.devtools.intel.com:8085/BBB
+kwinject $ASEVAL_GIT/sw_build_all.sh lib_only
+kwinject $ASEVAL_GIT/dummy_swbuild.sh
+
 kwcheck run --build-spec kwinject.out --local
 kwcheck list -F detailed --local --system --severity 1 > $ASEVAL_GIT/kw_run.1.log
 kwcheck list -F detailed --local --system --severity 2 > $ASEVAL_GIT/kw_run.2.log
