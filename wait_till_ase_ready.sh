698c6bebda598b84e30d1959f03e6107d2b53850
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-07-19 15:48:24 -0700

    Wait for 10 mins, or fail script

--- wait_till_ase_ready.sh
+++ wait_till_ase_ready.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-# Wait for Simulator to be running/ready
+max_timeout=600
+
+# Wait for Simulator to be running/ready (check for 10 mins = 600 sec)
 echo "Waiting for simulator to be ready ... "
-while [ ! -f $ASE_WORKDIR/.ase_ready.pid ]
+
+## Initialize timer
+let timer=0
+
+## Wait process
+while [ ! -f $ASE_WORKDIR/.ase_ready.pid ] && [ $timer -lt $max_timeout ]
 do
+    let "timer++"
+    echo "Waited $timer second(s)"
     sleep 1
 done
-echo "DONE"
 
+## Check if it actually ran
+if [ -f $ASE_WORKDIR/.ase_ready.pid ];
+then
+    echo "Simulator is running"
+    exit 0
+else
+    echo "Simulator is not running (failed after $max_timeout seconds"
+    exit -1
+fi
