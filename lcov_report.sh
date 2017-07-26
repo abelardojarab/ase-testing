2105cb092d88ad0b809972dd515d21333a0627bf
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-07-26 16:21:44 -0700

    LCOV scrub finished

--- lcov_report.sh
+++ lcov_report.sh
@@ -11,17 +11,17 @@ mkdir -p  $ASE_SRCDIR/coverage/
 # $ASEVAL_GIT/svunit_tests.sh ccip_checker_nlb
 $ASEVAL_GIT/coverage_generator.sh gtest
 $ASEVAL_GIT/coverage_generator.sh ccip_nlb_mode0
-# $ASEVAL_GIT/coverage_generator.sh ccip_mmio_rdwr_stress
+$ASEVAL_GIT/coverage_generator.sh ccip_mmio_rdwr_stress
 # $ASEVAL_GIT/coverage_generator.sh ccip_umsg_trigger
 
 ## Generate combined report
 cd $ASE_SRCDIR/coverage/
 lcov -a ccip_nlb_mode0.info \
+     -a ccip_mmio_rdwr_stress.info \
      -a gtest.info \
      -o raw.info
 
 #    -a ccip_umsg_trigger.info \
-#    -a ccip_mmio_rdwr_stress.info \
 
 lcov --remove raw.info 'common.c' 'safe_string/*' 'manage.c' 'reconf.c' 'umsg.c' 'error_report.c' 'event.c' --config-file $ASEVAL_GIT/lcovrc.cfg -o combined.info
 
