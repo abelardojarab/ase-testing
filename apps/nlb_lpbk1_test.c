09078dcc4e5b21a644e8956435143a31e2f0f39c
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-07-26 16:20:35 -0700

    add nlb_test, remove error inducement

--- apps/nlb_lpbk1_test.c
+++ apps/nlb_lpbk1_test.c
@@ -177,11 +177,6 @@ int main(int argc, char *argv[])
   printf("data32 = %08x\n", (uint32_t)data32);
   mmio_read32(AFU_ID_H + 4, &data32 );
   printf("data32 = %08x\n", (uint32_t)data32);
-
-#define ERR_TEST
-#ifdef ERR_TEST
-  raise(SIGSEGV);
-#endif
   
 #if 0
   mmio_read32(0x1020, &data32 );
