44d7cdc52c16bc2766e3703f89d77ae60a2a4310
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-07-27 11:04:15 -0700

    kill script cleanup

--- kill_running_ase.sh
+++ kill_running_ase.sh
@@ -2,27 +2,23 @@
 
 echo "ASE_WORKDIR =" $ASE_WORKDIR
 
+max_timeout=100
+let kill_timer=0
+
 # Check if ready file exists
 echo "Looking for ASE lock file in ASE_WORKDIR=" $ASE_WORKDIR
 if [ -e $ASE_WORKDIR/.ase_ready.pid ]
 then
     pid=`cat $ASE_WORKDIR/.ase_ready.pid | grep pid | cut -d "=" -s -f2-`
-    while [ -f $ASE_WORKDIR/.ase_ready.pid ]
+    while [ -f $ASE_WORKDIR/.ase_ready.pid ] && [ $kill_timer -lt $max_timeout ]
     do
 	echo "Killing Simulator PID " $pid
-	kill $pid
+ 	kill $pid
 	sleep 1
+	let "kill_timer++"
     done
+    echo "Killing wrapper make script"
+    kill $(ps -p `fuser $ASE_SRCDIR` | grep make | cut -f 1 -d " ")    
 else
     echo "Simulator has probably already been killed"
 fi
-
-# Disk cleanup
-# if [ $USER == "lab" ] ; then
-#     echo "####################################################"
-#     echo "# Proceeding with cleaning up simulation for space #"
-#     echo "####################################################"
-#     rm -rf $ASE_WORKDIR
-# else
-#     echo "Looks like a manual run, will NOT clean up simulation workspace"
-# fi
