15ddf808c3eab1522fd36f10006cd7cdf18363a9
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-08-02 13:51:49 -0700

    ccip_app_idiotproof run.sh added

--- test_afus/ccip_app_idiotproof/SW/run.sh
+++ test_afus/ccip_app_idiotproof/SW/run.sh
@@ -7,6 +7,7 @@ LD_LIBRARY_PATH=${MYINST_DIR}/lib/
 #####################################################################
 ## Start hello_fpga, kill immediately, check if send_simkill works ##
 #####################################################################
+echo "Starting App and killing within 2 seconds"
 cd $FPGASW_GIT/mybuild/bin/
 LD_PRELOAD=libopae-c-ase.so ./hello_fpga &
 
@@ -19,36 +20,52 @@ do
     echo "App is still running"
     sleep 1    
 done
-    
+echo "Test 1 complete"
+
+sleep 2
+
 #####################################################################
 ##            Try run two applications at one time                 ##
 #####################################################################
+echo "Running Two Apps at one time"
 cd $FPGASW_GIT/mybuild/bin/
-nohup LD_PRELOAD=libopae-c-ase.so ./hello_fpga & 
+echo "Starting App 1"
+LD_PRELOAD=libopae-c-ase.so ./hello_fpga | tee good_run.log & 
 good_pid=$!
 sleep 1
-LD_PRELOAD=libopae-c-ase.so ./hello_fpga &> bad_run.log
+echo "Starting App 2"
+LD_PRELOAD=libopae-c-ase.so ./hello_fpga | tee bad_run.log
 
 if grep -Fxq "ASE was found to be running with another application" bad_run.log
 then
     echo "Bad application failed to launch -- SUCCESS !"    
 else
-    echo "Test failed !"    
+    echo "Test 2 failed !"    
 fi
 
 wait
 
+sleep 2
+
 #####################################################################
 ##                  Try with no simulator running                  ##
 #####################################################################
-$ASEVAL_GIT/kill_running_ase.sh
-LD_PRELOAD=libopae-c-ase.so ./hello_fpga &> no_sim.log
+echo "Running App without a simulator"
+sim_pid=`cat $ASE_WORKDIR/.ase_ready.pid | grep pid | cut -d "=" -s -f2-`
+kill $sim_pid
+echo "  Simulator killed"
+sleep 2
+LD_PRELOAD=libopae-c-ase.so ./hello_fpga | tee no_sim.log
 if grep -Fxq "Simualtor is not running yet" no_sim.log
 then
     echo "Simulator finder (false case) worked correctly -- SUCCESS !"
 else
-    echo "Test Failed !"
+    echo "Test 3 Failed !"
 fi
 
 echo "Test Complete"
 
+#####################################################################
+##                  App Lock resilience test                       ##
+#####################################################################
+# TBD
