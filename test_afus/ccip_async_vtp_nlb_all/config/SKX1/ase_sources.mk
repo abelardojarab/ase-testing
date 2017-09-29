e94dc96b3129609051293db634d6378410bf925a
Rahul R Sharma <rahul.r.sharma@intel.com>
2017-09-29 12:04:31 -0700

    MPF test - update ase_sources

--- test_afus/ccip_async_vtp_nlb_all/config/SKX1/ase_sources.mk
+++ test_afus/ccip_async_vtp_nlb_all/config/SKX1/ase_sources.mk
@@ -9,33 +9,9 @@ DUT_VLOG_SRC_LIST = $(ASE_SRCDIR)/vlog_files.list
 
 DUT_INCDIR = $(BBB_GIT)/BBB_cci_mpf/rtl/+$(BBB_GIT)/BBB_cci_mpf/rtl/rtl+$(BBB_GIT)/BBB_cci_mpf/rtl/rtl/cci-mpf-shims+$(BBB_GIT)/BBB_cci_mpf/rtl/rtl/cci-mpf-shims/cci_mpf_shim_vtp+$(BBB_GIT)/BBB_cci_mpf/rtl/rtl/cci-mpf-shims/cci_mpf_shim_edge+$(BBB_GIT)/BBB_cci_mpf/rtl/rtl/cci-mpf-shims/cci_mpf_shim_wro+$(BBB_GIT)/BBB_cci_mpf/rtl/rtl/cci-mpf-shims/cci_mpf_shim_pwrite+$(BBB_GIT)/BBB_cci_mpf/rtl/rtl/cci-if+$(BBB_GIT)/BBB_cci_mpf/rtl/rtl/cci-mpf-if+$(BBB_GIT)/BBB_cci_mpf/rtl/rtl/cci-mpf-prims+$(BBB_GIT)/BBB_cci_mpf/rtl/par+$(BBB_GIT)/BBB_cci_mpf/sample/afu/+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/include_files+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/include_files/common+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/lpbk1_RdRspRAM2PORT+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/lpbk1_RdRspRAM2PORT/synth+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/lpbk1_RdRspRAM2PORT/ram_2port_160+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/lpbk1_RdRspRAM2PORT/ram_2port_160/synth+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/req_C1TxRAM2PORT+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/req_C1TxRAM2PORT/synth+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/req_C1TxRAM2PORT/ram_2port_160+$(ASEVAL_GIT)/test_afus/ccip_nlb_all_SKX1/HW/QSYS_IPs/RAM/req_C1TxRAM2PORT/ram_2port_160/synth+$(BBB_GIT)/BBB_ccip_async/rtl/rtl/+
 
-ASEHW_FILE_LIST = \
-	$(ASE_SRCDIR)/rtl/ccip_if_pkg.sv \
-	$(ASE_SRCDIR)/rtl/ase_pkg.sv \
-	$(ASE_SRCDIR)/rtl/outoforder_wrf_channel.sv \
-	$(ASE_SRCDIR)/rtl/latency_pipe.sv \
-	$(ASE_SRCDIR)/rtl/ccip_emulator.sv \
-	$(ASE_SRCDIR)/rtl/ase_svfifo.sv \
-	$(ASE_SRCDIR)/rtl/ccip_logger.sv \
-	$(ASE_SRCDIR)/rtl/ccip_checker.sv \
-	$(ASE_SRCDIR)/rtl/ase_top.sv \
-
-
-ASE_INCDIR = $(ASE_SRCDIR)/rtl/+
-
-ASESW_FILE_LIST = \
-	$(ASE_SRCDIR)/sw/ase_ops.c \
-	$(ASE_SRCDIR)/sw/ipc_mgmt_ops.c \
-	$(ASE_SRCDIR)/sw/mem_model.c \
-	$(ASE_SRCDIR)/sw/protocol_backend.c \
-	$(ASE_SRCDIR)/sw/tstamp_ops.c \
-	$(ASE_SRCDIR)/sw/mqueue_ops.c \
-	$(ASE_SRCDIR)/sw/error_report.c \
-	$(ASE_SRCDIR)/sw/linked_list_ops.c \
-	$(ASE_SRCDIR)/sw/randomness_control.c \
-
-
-ASE_TOP = ase_top
-
 SIMULATOR ?= VCS
+
+SNPS_VLOGAN_OPT = +define+VENDOR_ALTERA +define+TOOL_QUARTUS +define+NUM_AFUS=1 +define+CCIP_IF_V0_1 +define+MPF_PLATFORM_SKX  +define+CCI_SIMULATION=1
+
+ASE_PLATFORM = FPGA_PLATFORM_INTG_XEON
 
